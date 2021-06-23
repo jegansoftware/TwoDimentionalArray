4cb21f41f5e843c4c19a8a948a165c28afd673ac
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-23 03:16:50 -0700

    Create Jegan.java

---
+++ Jegan.java
@@ -0,0 +1,16 @@
+package love;
+
+public class Jegan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TwoDimensionArray i=new TwoDimensionArray();
+		i.Display();
+		i.sum();
+		i.min();
+		i.max();
+
+	}
+
+}
